fce61974b5b405885311daf348b4bd2d3e87b721
Grégoire Joncour <greg.joncour@gmail.com>
2018-05-03 01:31:02 +0200

    RER a conditions et retour Hack un peu hasardeux

--- api/Trains.js
+++ api/Trains.js
@@ -33,6 +33,60 @@ function getColorLigne(q) {
 	});
 }
 
+function getResultRER(sid, t, train, tripId) {
+	let dessertes = [];
+	let trip_infos;
+	let route_infos;
+	let line_infos;
+
+	return new Promise((resolve, reject) => {
+		const line = _.result(_.find(lignes, function(obj) {
+			return obj.uic === parseInt(t.term.toString().slice(0, -1));
+		}), 'line');
+
+		gtfs.getTrips({
+			agency_key: 'sncf-routes',
+			trip_id: tripId
+		})
+		.then(trip => {trip_infos = trip[0]})
+		.then(() => gtfs.getStoptimes({
+			agency_key: 'sncf-routes',
+			trip_id: tripId
+		}))
+		.then(stopTimes => {
+			dessertes = [];
+			_.forEach(stopTimes, (v, k) => {
+				const gareName = _.result(_.find(gares, function(obj) {
+					return obj.uic7 === parseInt(v.stop_id.replace("StopPoint:DUA",""));
+				}), 'nom_gare_sncf');
+				dessertes.push({uic7: parseInt(v.stop_id.replace("StopPoint:DUA","")), name: gareName, dep_time: moment(v.departure_time, "kk:mm:ss").format('LT')});
+			});
+		})
+		.then(() => gtfs.getRoutes({
+			agency_key: 'sncf-routes',
+			route_id: trip_infos.route_id
+		}))
+		.then(routes => route_infos = routes[0], () => resolve(['error route id']))
+		//.then(() => getColorLigne(route_infos.route_short_name))		//Get ligne color via url API https://data.sncf.com/api/records/1.0/search/?dataset=codes-couleur-des-lignes-transilien
+		//.then(toto => line_infos = toto )
+		.then(() => {
+			if(_.isEmpty(dessertes))
+				resolve(['error get desserte'])
+			else {
+				train.journey = dessertes;
+				train.route = {
+					id: route_infos.route_id,
+					line: route_infos.route_short_name,
+					long_name: route_infos.route_long_name,
+					color: "#"+route_infos.route_color
+				};
+				//train.line = line_infos;
+				resolve(train);
+			}
+		})
+	});
+}
+
 function getResultTrain(sid, t, train, service) {
 	let dessertes = [];
 	let trip_infos;
@@ -56,12 +110,11 @@ function getResultTrain(sid, t, train, service) {
 			//})
 
 		} else {
-			gtfs.getTrips(_.pickBy({
+			gtfs.getTrips({
 				agency_key: 'sncf-routes',
 				service_id: service.toString(),
-				trip_headsign: isNaN(train.number) ? train.name : null,
-				trip_id: !isNaN(train.number) ? {$regex: new RegExp(`DUASN${('0' + train.number).slice(-6)}`)} : null
-			}, _.identity))
+				trip_id: {$regex: new RegExp(`DUASN${('0' + train.number).slice(-6)}`)}
+			})
 			.then(results => paireVSimpaire(results, train, service))
 			.then(trip => {trip_infos = trip[0]}, () => resolve(['error trip id']))
 			.then(() => gtfs.getStoptimes({
@@ -134,8 +187,7 @@ function paireVSimpaire (results, train, service = null){
 			gtfs.getTrips(_.pickBy({
 				agency_key: 'sncf-routes',
 				service_id: service ? service.toString() : null,
-				trip_headsign: isNaN(train.number) ? train.name : null,
-				trip_id: !isNaN(train.number) ? {$regex: new RegExp(`DUASN${('0' + (train.number % 2 == 1 ? train.number - 1 : train.number)).slice(-6)}`)} : null
+				trip_id: {$regex: new RegExp(`DUASN${('0' + (train.number % 2 == 1 ? train.number - 1 : train.number)).slice(-6)}`)}
 			}, _.identity))
 			.then(response => resolve(response))
 		} else {
@@ -181,14 +233,14 @@ function getService(t, sid) {
 		})
 		.then(opt => gtfs.getCalendars(opt))
 		.then(calendars => {
-			/*if(_.isEmpty(calendars) && !_.isEmpty(services))
-				services_i = services;
-			else*/
-			services_i = [];
-			_.forEach(calendars, (v,k) => {
-				services_i.push(v.service_id);
-			});
-			//if(train.name == "UZAN") console.log(calendars)
+			//if(_.isEmpty(calendars) && !_.isEmpty(services))
+			//	services_i = _.uniqWith(services, _.isEqual);
+			//else {
+				services_i = [];
+				_.forEach(calendars, (v,k) => {
+					services_i.push(v.service_id);
+				});
+			//}
 		})
 		.then(() => gtfs.getCalendarDates({
 			agency_key: 'sncf-routes',
@@ -202,7 +254,7 @@ function getService(t, sid) {
 						services_i = [v.service_id];
 						return false;
 					} else {
-						services_i = v.service_id;
+						services_i = _.uniqWith(services_i.concat(v.service_id), _.isEqual);
 					}
 				} else if (v.exception_type === 2) {
 					var index = services_i.indexOf(v.service_id);
@@ -210,11 +262,36 @@ function getService(t, sid) {
 						services_i.splice(index, 1);
 					}
 				}
-			});
-			if(train.name == "UZAN") console.log(services_i)
+			});		
 			return services_i;
 		})
-		.then(service => getResultTrain(sid, t, train, service))
+		.then(service => new Promise(resolve => {
+				if(isNaN(train.number)){
+					gtfs.getStoptimes({
+						agency_key: 'sncf-routes',
+						stop_id: "StopPoint:DUA"+sid,
+						service_id: {$in: service},
+						departure_time: {
+							$lte: moment(train.expectedDepartureTime).format("HH:mm:ss"),
+							$gte: moment(train.expectedDepartureTime).subtract(5, 'm').format("HH:mm:ss")
+						}
+					})
+					.then(stoptimes => {
+						if(!_.isEmpty(stoptimes)){
+							train.aimedDepartureTime = moment(stoptimes[0].departure_time, "kk:mm:ss"); //kk heure format 01-24 à la plce de HH 00-23
+							if(moment(train.aimedDepartureTime).diff(moment(train.expectedDepartureTime), 'd') > 0){ // verifications horaires chevauchement entre deux jours
+								train.aimedDepartureTime = moment(stoptimes[0].departure_time, "kk:mm:ss").add(1, 'd');
+							}
+							console.log(train.number, stoptimes[0].trip_id);
+							resolve(getResultRER(sid, t, train, stoptimes[0].trip_id))
+						}
+					})
+				} else {
+					console.log(train.number, service)
+					resolve(getResultTrain(sid, t, train, service))
+				}
+			})
+		)
 		.then(resp => resolve(resp))
 	});
 };
